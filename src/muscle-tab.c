4f6fcf866da58ca5685706176a2406fd3bc9377c
Akim Demaille <akim@lrde.epita.fr>
2012-12-01 18:46:15 +0100

    backward compatibility
    * src/muscle-tab.c (muscle_percent_variable_update): Accept lex_symbol.

--- src/muscle-tab.c
+++ src/muscle-tab.c
@@ -400,6 +400,7 @@ muscle_percent_variable_update (char const *variable, location variable_loc)
       { "api.push_pull", "api.push-pull", },
       { "api.tokens.prefix", "api.token.prefix", },
       { "location_type", "api.location.type", },
+      { "lex_symbol", "api.token.constructor", },
       { "lr.default-reductions", "lr.default-reduction", },
       { "lr.keep-unreachable-states", "lr.keep-unreachable-state", },
       { "lr.keep_unreachable_states", "lr.keep-unreachable-state", },
