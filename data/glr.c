fb08f429ffaf4725cb1972e0d697267f76df1c90
Akim Demaille <akim@lrde.epita.fr>
2012-12-03 14:41:25 +0100

    glr.cc: be sure to iniitialize the yylloc
    * data/glr.c: here.

--- data/glr.c
+++ data/glr.c
@@ -260,8 +260,8 @@ b4_percent_code_get([[top]])[
    here we set the default value of $$ to a zeroed-out value.
    Since the default value is undefined, this behavior is
    technically correct.  */
-static YYSTYPE yyval_default;]b4_locations_if([[
-static YYLTYPE yyloc_default][]b4_yyloc_default;]))[
+static YYSTYPE yyval_default;])[]b4_locations_if([[
+static YYLTYPE yyloc_default][]b4_yyloc_default;])[
 
 /* Copy the second part of user declarations.  */
 ]b4_user_post_prologue
